544e5b6046377587e1ee66d1ab891a907d6d832c
paulR <prichert17@gmail.com>
2026-01-27 09:47:17 +0100

    suppression des projets de test inutiles

--- BLE/build/BLE/tfm/api_ns/platform/common/core/config_nordic_nrf_spe.cmake
+++
@@ -1,8 +0,0 @@
-#
-# Copyright (c) 2023, Nordic Semiconductor ASA.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-#
-
-set(HAL_NORDIC_PATH C:/ncs/v3.2.1/modules/hal/nordic CACHE STRING "Path to nordic HAL" FORCE)
-set(HAS_RRAMC 1 CACHE BOOL "Device uses RRAMC instead of NVMC" FORCE)
