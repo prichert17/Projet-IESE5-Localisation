ef8f9a8174a925ebd54820ece7ceac723a9f4cee
paulR <prichert17@gmail.com>
2026-01-27 09:42:12 +0100

    suppression des anciens projets de test

--- BLE/build/BLE/tfm/CMakeFiles/clean_additional.cmake
+++
@@ -1,9 +0,0 @@
-# Additional clean files
-cmake_minimum_required(VERSION 3.16)
-
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "MinSizeRel")
-  file(REMOVE_RECURSE
-  "bin\\tfm_s.map"
-  "generated"
-  )
-endif()
